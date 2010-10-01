67b91ffe617b8f8472140c0bf269932039f6ecca
Ben McGraw <mcgrue@verge-rpg.com>
2010-10-01 12:22:08 -0700

    cakey shell

---
+++ app/config/database.php
@@ -0,0 +1,14 @@
+<?php
+class DATABASE_CONFIG {
+
+	var $default = array(
+		'driver' => 'mysql',
+		'persistent' => true,
+		'host' => 'localhost',
+		'login' => 'root',
+		'password' => '',
+		'database' => 'sexymans',
+		'encoding' => 'UTF-8'
+	);
+}
+?>
