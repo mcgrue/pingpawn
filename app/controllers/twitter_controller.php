a45a7b66ab2f425b78622f680f626641cda8efb7
Ben McGraw <mcgrue@verge-rpg.com>
2010-10-12 17:41:37 -0700

    fixing twitter example

--- app/controllers/twitter_controller.php
+++ app/controllers/twitter_controller.php
@@ -9,23 +9,22 @@ class TwitterController extends AppController {
     var $components = array('RequestHandler', 'Cookie', 'OauthConsumer');
 
     public function twitter() {
-        
-        
-        if( is_localhost() ) {
+
+        if( is_sandbox() ) {
             $response_url = 'http://localhost/pingpawn/twitter/twitter_callback';    
         } else {
             $response_url = 'http://www.pingpawn.com/twitter/twitter_callback';    
         }
         
         $requestToken = $this->OauthConsumer->getRequestToken('Twitter', 'http://twitter.com/oauth/request_token', $response_url );
+
         $this->Session->write('twitter_request_token', $requestToken);
         $this->redirect('http://twitter.com/oauth/authorize?oauth_token=' . $requestToken->key);
     }
 
     public function twitter_callback() {
-        $requestToken = $this->Session->read('twitter_request_token');
+        $requestToken = $this->Session->read('twitter_request_token');        
         $accessToken = $this->OauthConsumer->getAccessToken('Twitter', 'http://twitter.com/oauth/access_token', $requestToken);
-
         $this->OauthConsumer->post('Twitter', $accessToken->key, $accessToken->secret, 'http://twitter.com/statuses/update.json', array('status' => 'hello world!'));
     }
 }
