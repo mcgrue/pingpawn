506e7ea648bb90692a622f423355a8575e51765d
Ben McGraw <mcgrue@verge-rpg.com>
2010-10-12 17:27:13 -0700

    adding oauth components courtesy of http://code.42dh.com/oauth/

---
+++ app/controllers/components/oauth_consumers/twitter_consumer.php
@@ -0,0 +1,8 @@
+<?
+
+class TwitterConsumer extends AbstractConsumer {
+    public function __construct() {
+        /// these functions hidden away in the gitignore'd database.php
+        parent::__construct(get_oauth_consumer_key(), get_oauth_consumer_secret());
+    }
+}
