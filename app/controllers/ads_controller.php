616e95b0e6a946ba4d96598481af0722ad10fa36
Ben McGraw <mcgrue@verge-rpg.com>
2010-10-13 04:38:23 -0700

    ads complete?

--- app/controllers/ads_controller.php
+++ app/controllers/ads_controller.php
@@ -1,8 +1,12 @@
 <?
 
-class AdController extends AppController {
+class AdsController extends AppController {
+
+    var $name = 'Ads';
+    var $uses = array();
 
     function index() {
-        
+        echo('<html><body style="background: transparent;"></body></html>');
+        die();
     }
 }
