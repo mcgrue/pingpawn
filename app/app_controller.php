a5408828ed933a4f871f5653a29dcd74e4dd9d39
Ben McGraw <mcgrue@verge-rpg.com>
2010-10-14 15:58:20 -0700

    pingpawn.com is now canonical.

--- app/app_controller.php
+++ app/app_controller.php
@@ -8,6 +8,15 @@
 		);
         
         function beforeFilter() {
+            if( !is_sandbox() ) {
+                if( $_SERVER['HTTP_HOST'] != 'pingpawn.com' ) {
+                    
+                    Header( "HTTP/1.1 301 Moved Permanently" );
+                    header( 'Location: http://pingpawn.com'.$_SERVER['REQUEST_URI'] );
+                    die();
+                }
+            }
+            
             parent::beforeFilter();
             $this->sessuser = Authsome::get();
             $this->set('sessuser', $this->sessuser);
