1b7ce7ed36c954d8c8724b163735ab8b219793d6
Ben McGraw <mcgrue@verge-rpg.com>
2010-10-17 23:35:55 -0700

    proof of concept use of getachievements.

--- app/app_controller.php
+++ app/app_controller.php
@@ -1,29 +1,30 @@
 <?
-	class AppController extends Controller {
-		public $components = array(
-            'Session',
-			'Authsome.Authsome' => array(
-				'model' => 'User'
-			)
-		);
-        
-        function beforeFilter() {
-            if( !is_sandbox() ) {
-                if( $_SERVER['HTTP_HOST'] != 'pingpawn.com' ) {
-                    
-                    Header( "HTTP/1.1 301 Moved Permanently" );
-                    header( 'Location: http://pingpawn.com'.$_SERVER['REQUEST_URI'] );
-                    die();
-                }
+
+class AppController extends Controller {
+    public $components = array(
+        'Session',
+        'Authsome.Authsome' => array(
+            'model' => 'User'
+        )
+    );
+    
+    function beforeFilter() {
+        if( !is_sandbox() ) {
+            if( $_SERVER['HTTP_HOST'] != 'pingpawn.com' ) {
+                
+                Header( "HTTP/1.1 301 Moved Permanently" );
+                header( 'Location: http://pingpawn.com'.$_SERVER['REQUEST_URI'] );
+                die();
             }
-            
-            parent::beforeFilter();
-            $this->sessuser = Authsome::get();
-            $this->set('sessuser', $this->sessuser);
         }
         
-        function flashAndGo( $flash, $go, $response=302 ) {
-            $this->Session->setFlash($flash);
-            $this->redirect($go, $response);            
-        }
+        parent::beforeFilter();
+        $this->sessuser = Authsome::get();
+        $this->set('sessuser', $this->sessuser);
     }
+    
+    function flashAndGo( $flash, $go, $response=302 ) {
+        $this->Session->setFlash($flash);
+        $this->redirect($go, $response);            
+    }
+}
--- app/views/layouts/default.ctp
+++ app/views/layouts/default.ctp
@@ -247,5 +247,9 @@
     <div id="pending">&nbsp;</div>
 <? endif; ?>
 
+<? if(!empty($sessuser)): ?>
+<script type="text/javascript" src="http://www.getachievements.com/unlock/notifier.js?set=4db58145f7f070ecf66437d25c358fc7&email_hash=<?=get_achievements_md5($sessuser)  ?>"></script>
+<? endif; ?>
+
 </body>
 </html>
