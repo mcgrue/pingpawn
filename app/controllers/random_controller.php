4dc6003c2123d7b56c5065f825469b74d446726f
Ben McGraw <mcgrue@verge-rpg.com>
2010-10-21 18:54:13 -0700

    fixing random.

--- app/controllers/random_controller.php
+++ app/controllers/random_controller.php
@@ -5,7 +5,7 @@ class RandomController extends AppController {
     var $uses = array('Quote');
     
 	function index() {
-        $res = $this->Quote->findAllByPublic(1,null,'rand()',1,null,null);
+        $res = $this->Quote->findAllByIsPublic(1,null,'rand()',1,null,null);
         
         if( isset($res[0]['Quote']['id']) ) {
             $this->redirect(array('controller'=>'quotes', 'action'=>'index', $res[0]['Quote']['id']) );
