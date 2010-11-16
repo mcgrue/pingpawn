7dc8e718a9d0b586a74d7e2d2076ba946c88c062
Ben McGraw <mcgrue@verge-rpg.com>
2010-11-15 22:48:08 -0800

    Adding search to api.

--- app/controllers/api_controller.php
+++ app/controllers/api_controller.php
@@ -3,21 +3,7 @@ class ApiController extends AppController {
 
 	var $name = 'Api';
     
-    var $helpers = array('Js');
-
     var $uses = array('Quote');
-
-
-    function index() {
-        $data = array(
-            'foo' => 'bar',
-            'baz' => 'bat',
-        );
-        
-        $json = json_encode($data);
-        echo $json;
-        die();
-    }
 
     function _output($res=NULL) {
         if(empty($res[0])) {
@@ -34,6 +20,22 @@ class ApiController extends AppController {
         } else {
             $from = mysql_real_escape_string($from);
             $res = $this->Quote->query( "SELECT q.* FROM `quotes` q, `prfs` p WHERE p.name = '$from' AND p.id = q.prf_id AND q.is_public = 1 ORDER BY RAND() LIMIT 1" );
+        }
+        $this->_output($res);
+    }
+
+    function search($from=NULL, $idx=NULL) {
+        $res = NULL;
+        if(!empty($_GET['q'])) {
+            $q = mysql_real_escape_string($_GET['q']);
+            $q = str_replace( '*', '%', $q );
+            
+            if($from===NULL) {
+                $res = $this->Quote->query( "SELECT * FROM `quotes` WHERE is_public = 1 AND original_quote LIKE '%$q%' ORDER BY RAND() LIMIT 1" );
+            } else {
+                $from = mysql_real_escape_string($from);
+                $res = $this->Quote->query( "SELECT q.* FROM `quotes` q, `prfs` p WHERE p.name = '$from' AND q.original_quote LIKE '%$q%' AND p.id = q.prf_id AND q.is_public = 1 ORDER BY RAND() LIMIT 1" );
+            }
         }
         $this->_output($res);
     }
